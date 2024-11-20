af4ae7193964498073e6612fb60b4a005c62ced2
Cheman002 <Cheman002@gmail.com>
2024-11-20 18:39:42 +0530

    portfolio

---
+++ aos.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.wel').attr("data-aos-delay","300");
+    $('.wel').attr('data-aos-duration',"1000");
+    /* $('.my-img').attr('data-aos','fade-down');
+    $('.my-img').attr('data-aos-delay','200');
+    $('.my-img').attr('data-aos-duration','1000');
+    */
+  });
